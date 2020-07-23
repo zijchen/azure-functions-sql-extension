3270ebc461c7f40945b53214670f8f017a0cad1e
Sophia Tevosyan <t-sotevo@microsoft.com>
2020-07-22 19:10:29 -0700

    output working too

--- samples/python/SqlOutput/main.py
+++ samples/python/SqlOutput/main.py
@@ -2,5 +2,6 @@ import logging
 import azure.functions
 
 def main(req: azure.functions.HttpRequest, row : azure.functions.Out[str]) -> str:
-    row = "{\"ProductID\":" + str(1) + ",\"Name\":" + "\"" + "Bottle" + "\"" + ",\"Cost\":" + str(10) + "}"
-    return row
+    string_row = "{\"ProductID\":" + str(1) + ",\"Name\":" + "\"" + "Bottle" + "\"" + ",\"Cost\":" + str(10) + "}"
+    row.set(string_row)
+    return string_row
